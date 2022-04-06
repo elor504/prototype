d594d5cf12a9e92e38e6735e1c1eacb5b5364950
JinMoshchenko <jinmoshchenko@gmail.com>
2022-04-06 20:35:57 +0300

    dsa

---
+++ Assets/Scripts/Jenya_Scripts/MenusInteractions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenusInteractions : MonoBehaviour
+{
+    
+
+
+
+
+
+    public void PlayB()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LevelSelectionB()
+    {
+        
+    }
+    public void SettingsB()
+    {
+        
+    }
+    public void CreditsB()
+    {
+        
+    }
+}
