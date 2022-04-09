da370a49ee134bfbd6ebdf6b685a2b1500df72cf
JinMoshchenko <jinmoshchenko@gmail.com>
2022-04-09 15:53:27 +0300

    pog

--- Assets/Scripts/Jenya_Scripts/MenusInteractions.cs
+++ Assets/Scripts/Jenya_Scripts/MenusInteractions.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 
 public class MenusInteractions : MonoBehaviour
 {
-    
+    private int lastPlayedLevel;
 
     void Start()
     {
         var context = new MenuContext(new StateMainMenu());
         context.Request();
+
+        // Get the data from last scene
+        lastPlayedLevel = 1;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(InterfaceHandler.GetInstance.uiState != UIState.MainMenu)
+            {
+                BackB();
+            }
+        }
     }
 
 
 
     public void PlayB()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(lastPlayedLevel);
     }
     public void LevelSelectionB()
     {
@@ -35,5 +48,26 @@ public class MenusInteractions : MonoBehaviour
     public void QuitB()
     {
         Application.Quit();
+    }
+    public void BackB()
+    {
+        var context = new MenuContext(new StateMainMenu());
+        context.Request();
+    }
+    public void TutorialB()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Level1B()
+    {
+        //SceneManager.LoadScene(2);
+    }
+    public void Level2B()
+    {
+        //SceneManager.LoadScene(3);
+    }
+    public void Level3B()
+    {
+        //SceneManager.LoadScene(4);
     }
 }
