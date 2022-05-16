868947e39b01341cbc3f49724cff2a51a3d2055d
Roza13-bit <roizamir876@gmail.com>
2022-05-16 21:00:50 +0300

    Line GFX Fix

---
+++ Assets/LineGFXManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGFXManager : MonoBehaviour
+{
+    [SerializeField] LineRenderer mainLine;
+    [SerializeField] LineRenderer prefabGFXLine;
+
+    private int lineDots = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Instantiate(prefabGFXLine);
+        prefabGFXLine.SetPosition(0, mainLine.GetPosition(0));
+        prefabGFXLine.SetPosition(1, mainLine.GetPosition(1));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mainLine.positionCount == 2)
+        {
+            prefabGFXLine.SetPosition(0, mainLine.GetPosition(0));
+            prefabGFXLine.SetPosition(1, mainLine.GetPosition(1));
+
+        }
+        
+
+    }
+
+    private void OnLinePosAdded()
+    {
+        
+
+    }
+
+}
