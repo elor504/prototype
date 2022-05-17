34c71ae0a8b4897af278616d56029805a00947b0
Roza13-bit <roizamir876@gmail.com>
2022-05-17 17:54:03 +0300

    Line GFX Almost done.

--- Assets/LineGFXManager.cs
+++ Assets/LineGFXManager.cs
@@ -8,33 +8,63 @@ public class LineGFXManager : MonoBehaviour
     [SerializeField] LineRenderer mainLine;
     [SerializeField] LineRenderer prefabGFXLine;
 
-    private int lineDots = 2;
+    [SerializeField] private int lineDots = 0;
+    private LineRenderer activeGFXLine;
+    [SerializeField] List<LineRenderer> lineGFXList = new List<LineRenderer>();
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Instantiate(prefabGFXLine);
-        prefabGFXLine.SetPosition(0, mainLine.GetPosition(0));
-        prefabGFXLine.SetPosition(1, mainLine.GetPosition(1));
-
+        lineDots = 0;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mainLine.positionCount == 2)
-        {
-            prefabGFXLine.SetPosition(0, mainLine.GetPosition(0));
-            prefabGFXLine.SetPosition(1, mainLine.GetPosition(1));
-
-        }
-        
-
+        OnLinePosAdded();
     }
 
     private void OnLinePosAdded()
     {
         
+        if (mainLine.positionCount <= lineDots)
+        {
+            if (mainLine.positionCount != 0)
+            {
+                if (lineGFXList.Count +1 < mainLine.positionCount)
+                {
+                    Debug.Log("created line");
+
+                    if (lineGFXList.Count != 0)
+                    {
+                        activeGFXLine.SetPosition(1, mainLine.GetPosition(lineDots-2));
+                    }
+
+                    activeGFXLine = GameObject.Instantiate(prefabGFXLine);
+
+                    lineGFXList.Add(activeGFXLine);
+
+                }
+
+                activeGFXLine.SetPosition(0, mainLine.GetPosition(lineDots - 2));
+                activeGFXLine.SetPosition(1, mainLine.GetPosition(lineDots - 1));
+
+            }
+            else if (mainLine.positionCount == 0)
+            {
+                foreach (LineRenderer item in lineGFXList)
+                {
+                    Destroy(item);
+                }
+
+                lineGFXList.Clear();
+
+            }
+
+        }
+
+        lineDots = mainLine.positionCount;
 
     }
 
