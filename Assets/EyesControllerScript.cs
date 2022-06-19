84d55334465965873471d58d13bf3a0a18f6f135
Roza13-bit <roizamir876@gmail.com>
2022-06-19 16:15:18 +0300

    Eyez start

---
+++ Assets/EyesControllerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesControllerScript : MonoBehaviour
+{
+    [SerializeField] Transform pupilTranform;
+    [SerializeField] Transform eyeTranform;
+
+    [SerializeField] Transform ghosyBoyTranform;
+    public float eyeOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.right = ghosyBoyTranform.position - eyeTranform.position;
+
+
+    }
+
+}
