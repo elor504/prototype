dfcfecab852f343246f5c68a2028d071e85d2591
JinMoshchenko <jinmoshchenko@gmail.com>
2022-07-30 16:44:54 +0300

    fds

--- Assets/Scripts/Jenya_Scripts/Menus/SettingsToggleLight.cs
+++ Assets/Scripts/Jenya_Scripts/Menus/SettingsToggleLight.cs
@@ -5,6 +5,7 @@ public class SettingsToggleLight : MonoBehaviour
 {
     public Toggle toggle;
     public GameObject toggleLampMask;
+    public GameObject dropdownItemHighlight;
 
 
     public void ChangeToggleVisuals()
@@ -16,6 +17,18 @@ public class SettingsToggleLight : MonoBehaviour
         else
         {
             toggleLampMask.SetActive(false);
+        }
+    }
+
+    public void ChangeItemHighlight(bool show)
+    {
+        if (show)
+        {
+            dropdownItemHighlight.SetActive(true);
+        }
+        else
+        {
+            dropdownItemHighlight.SetActive(false);
         }
     }
 }
