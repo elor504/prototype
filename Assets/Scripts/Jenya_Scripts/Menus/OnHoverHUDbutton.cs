3e94da9aadc2875598f21a08e0feef73c8299516
JinMoshchenko <jinmoshchenko@gmail.com>
2022-07-30 17:08:24 +0300

    adssa

---
+++ Assets/Scripts/Jenya_Scripts/Menus/OnHoverHUDbutton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHoverHUDbutton : MonoBehaviour
+{
+    Color newColor;
+
+
+
+
+    void OnMouseOver()
+    {
+        newColor = new Color(3f, 45f, 67f);
+        gameObject.GetComponentInChildren<TextMesh>().color = newColor;
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponentInChildren<TextMesh>().color = Color.white;
+    }
+}
